cd7977b861adacdb63ac1d765de7000ec3c46562
IgorC4rd0s0 <igorgoncalvescardoso@gmail.com>
2022-11-25 11:57:50 -0300

    correção

--- ex03/scripts.js
+++ ex03/scripts.js
@@ -1,12 +1,8 @@
 document.write("EXERCÍCIO 03" + "<br/>" + "<br/>");
-let soma, contPar
 let num = prompt ("Informe um numero")
-while (num == 0 )
-{
-    window.alert ("Informe o numero novamente")
+let resto = new Array()
 
-    if (num %2 == 0)
-    contPar = contPar + 1
+while (num %2 == 0) {
+    resto.push (num)
 }
-document.write("Quantidade de números Pares digitados: " + contPar + "<br/>" + "<br/>")
-document.write("Soma dos números digitados: " + soma + "<br/>" + "<br/>")
+document.write (resto)
